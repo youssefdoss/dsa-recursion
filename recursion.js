6ff0aee90894920642a65e19d465d9148b381f55
Youssef Doss <youssefdoss97@gmail.com>
2023-02-07 14:47:18 -0500

    initial commit

---
+++ recursion.js
@@ -0,0 +1,84 @@
+/** product: calculate the product of an array of numbers. */
+
+function product(nums) {
+
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words) {
+
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str) {
+
+}
+
+/** find: return boolean depending on if val exists in array or not. */
+
+function find(arr, val) {
+
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str) {
+
+}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str) {
+
+}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val) {
+
+}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {
+
+}
+
+// FURTHER STUDY
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return true if val is in array, false if not present). */
+
+function binarySearch(arr, val) {
+
+}
+
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearchIndex(arr, val) {
+
+}
+
+// you might find the above two problems easier if you change the function signature to:
+//
+// function binarySearch(arr, val, left = 0, right = arr.length) {
+//
+// }
+
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  find,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch,
+  binarySearchIndex,
+};
