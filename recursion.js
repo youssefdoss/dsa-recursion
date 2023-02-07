cc6cad21e0766189c30738f5fa6550bc0b89af00
Youssef Doss <youssefdoss97@gmail.com>
2023-02-07 15:13:58 -0500

    through questions

--- recursion.js
+++ recursion.js
@@ -1,49 +1,85 @@
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
+  if (nums.length === 0) return 1;
 
+  return nums[0] * product(nums.slice(1));
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-
+  if (words.length === 0) return 0;
+  const currLength = words[0].length;
+  return Math.max(currLength, longest(words.slice(1)));
 }
 
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
+  if (str === '') return '';
 
+  return str[0] + everyOther(str.slice(2));
 }
 
 /** find: return boolean depending on if val exists in array or not. */
 
 function find(arr, val) {
+  if (arr.length === 0) return false;
+  if (arr[0] == val) return true;
 
+  return find(arr.slice(1), val);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length -1]) return false;
 
+  return isPalindrome(str.slice(1, str.length - 1));
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str) {
+  if (str === '') return '';
 
+  return revString(str.slice(1)) + str[0];
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+function findIndex(arr, val, idx = 0) {
+  if (arr.length === idx) return -1;
+  if (arr[idx] === val) return idx;
+  return findIndex(arr, val, idx + 1);
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
+function gatherStrings(obj, strings = []) {
+  // Alternate option without strings passed in as parameter
+  // let strings = [];
+  // for (let key in obj) {
+  //   if (typeof obj[key] === 'object') {
+  //     strings.push(...gatherStrings(obj[key]));
+  //   }
+  //   if (typeof obj[key] === 'string') {
+  //     strings.push(obj[key]);
+  //   }
+  // }
+  // return strings;
 
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      gatherStrings(obj[key], strings);
+    }
+    if (typeof obj[key] === 'string') {
+      strings.push(obj[key]);
+    }
+  }
+  return strings;
 }
 
 // FURTHER STUDY
